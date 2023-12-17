914c3a5e910c7dd1d3947986380940fda1529ba2
Abdul Wahab <86112828+abdulwahab-codehub@users.noreply.github.com>
2023-12-18 00:26:01 +0500

    Add files via upload

--- js/compass.js
+++ js/compass.js
@@ -1,424 +1,128 @@
-(function () {
-  "use strict";
+const compass = document.getElementById('compass');
+        const msg = document.getElementById('msg');
+        const directionDisplay = document.getElementById('angle');
+        const locationElem = document.getElementById('location');
+        const coordsElem = document.getElementById('coords');
+        const shareLinkElem = document.getElementById('share-link');
+        const locationSwitch = document.getElementById("locationSwitch");
 
-  //set to true for debugging output
-  var debug = false;
+        let simulatedAngle = 0;
 
-  // our current position
-  var positionCurrent = {
-    lat: null,
-    lng: null,
-    hng: null
-  };
-
-
-  // the outer part of the compass that rotates
-  var rose = document.getElementById("rose");
-
-
-  // elements that ouput our position
-  var positionLat = document.getElementById("position-lat");
-  var positionLng = document.getElementById("position-lng");
-  var positionHng = document.getElementById("position-hng");
-
-
-  // debug outputs
-  var debugOrientation = document.getElementById("debug-orientation");
-  var debugOrientationDefault = document.getElementById("debug-orientation-default");
-
-
-  // info popup elements, pus buttons that open popups
-  var popup = document.getElementById("popup");
-  var popupContents = document.getElementById("popup-contents");
-  var popupInners = document.querySelectorAll(".popup__inner");
-  var btnsPopup = document.querySelectorAll(".btn-popup");
-
-
-  // buttons at the bottom of the screen
-  var btnLockOrientation = document.getElementById("btn-lock-orientation");
-  var btnNightmode = document.getElementById("btn-nightmode");
-  var btnMap = document.getElementById("btn-map");
-  var btnInfo = document.getElementById("btn-info");
-
-
-  // if we have shown the heading unavailable warning yet
-  var warningHeadingShown = false;
-
-
-  // switches keeping track of our current app state
-  var isOrientationLockable = false;
-  var isOrientationLocked = false;
-  var isNightMode = false;
-
-
-  // the orientation of the device on app load
-  var defaultOrientation;
-
-
-  // browser agnostic orientation
-  function getBrowserOrientation() {
-    var orientation;
-    if (screen.orientation && screen.orientation.type) {
-      orientation = screen.orientation.type;
-    } else {
-      orientation = screen.orientation ||
-                    screen.mozOrientation ||
-                    screen.msOrientation;
-    }
-
-    /*
-      'portait-primary':      for (screen width < screen height, e.g. phone, phablet, small tablet)
-                                device is in 'normal' orientation
-                              for (screen width > screen height, e.g. large tablet, laptop)
-                                device has been turned 90deg clockwise from normal
-
-      'portait-secondary':    for (screen width < screen height)
-                                device has been turned 180deg from normal
-                              for (screen width > screen height)
-                                device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
-
-      'landscape-primary':    for (screen width < screen height)
-                                device has been turned 90deg clockwise from normal
-                              for (screen width > screen height)
-                                device is in 'normal' orientation
-
-      'landscape-secondary':  for (screen width < screen height)
-                                device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
-                              for (screen width > screen height)
-                                device has been turned 180deg from normal
-    */
-
-    return orientation;
-  }
-
-
-  // browser agnostic orientation unlock
-  function browserUnlockOrientation() {
-    if (screen.orientation && screen.orientation.unlock) {
-      screen.orientation.unlock();
-    } else if (screen.unlockOrientation) {
-      screen.unlockOrientation();
-    } else if (screen.mozUnlockOrientation) {
-      screen.mozUnlockOrientation();
-    } else if (screen.msUnlockOrientation) {
-      screen.msUnlockOrientation();
-    }
-  }
-
-
-  // browser agnostic document.fullscreenElement
-  function getBrowserFullscreenElement() {
-    if (typeof document.fullscreenElement !== "undefined") {
-      return document.fullscreenElement;
-    } else if (typeof document.webkitFullscreenElement !== "undefined") {
-      return document.webkitFullscreenElement;
-    } else if (typeof document.mozFullScreenElement !== "undefined") {
-      return document.mozFullScreenElement;
-    } else if (typeof document.msFullscreenElement !== "undefined") {
-      return document.msFullscreenElement;
-    }
-  }
-
-
-  // browser agnostic document.documentElement.requestFullscreen
-  function browserRequestFullscreen() {
-    if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-      document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-      document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.msRequestFullscreen) {
-      document.documentElement.msRequestFullscreen();
-    }
-  }
-
-
-  // browser agnostic document.documentElement.exitFullscreen
-  function browserExitFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-  }
-
-
-  // called on device orientation change
-  function onHeadingChange(event) {
-    var heading = event.alpha;
-
-    if (typeof event.webkitCompassHeading !== "undefined") {
-      heading = event.webkitCompassHeading; //iOS non-standard
-    }
-
-    var orientation = getBrowserOrientation();
-
-    if (typeof heading !== "undefined" && heading !== null) { // && typeof orientation !== "undefined") {
-      // we have a browser that reports device heading and orientation
-
-
-      if (debug) {
-        debugOrientation.textContent = orientation;
-      }
-
-
-      // what adjustment we have to add to rotation to allow for current device orientation
-      var adjustment = 0;
-      if (defaultOrientation === "landscape") {
-        adjustment -= 90;
-      }
-
-      if (typeof orientation !== "undefined") {
-        var currentOrientation = orientation.split("-");
-
-        if (defaultOrientation !== currentOrientation[0]) {
-          if (defaultOrientation === "landscape") {
-            adjustment -= 270;
-          } else {
-            adjustment -= 90;
-          }
+        function isAppleDevice() {
+            return [
+                    "iPad Simulator", "iPhone Simulator", "iPod Simulator",
+                    "iPad", "iPhone", "iPod"
+                ].includes(navigator.platform) ||
+                (navigator.userAgent.includes("Mac") && "ontouchend" in document);
         }
 
-        if (currentOrientation[1] === "secondary") {
-          adjustment -= 180;
+        function handleOrientationEvent(e) {
+            let calculatedHeading;
+            if (e.webkitCompassHeading) {
+                calculatedHeading = e.webkitCompassHeading - 180;
+            } else {
+                calculatedHeading = 180 + e.alpha;
+            }
+            calculatedHeading -= 180;
+            if (calculatedHeading > 360) calculatedHeading -= 360;
+            setCompassDirection(calculatedHeading);
         }
-      }
 
-      positionCurrent.hng = heading + adjustment;
+        function setCompassDirection(angle) {
+            directionDisplay.innerHTML = getDirectionLabel(angle) + "  " + Math.round((360 - angle) * 10) / 10 + "&deg;";
+            compass.style.transform = "rotate(" + angle + "deg)";
+        }
 
-      var phase = positionCurrent.hng < 0 ? 360 + positionCurrent.hng : positionCurrent.hng;
-      positionHng.textContent = (360 - phase | 0) + "°";
+        function getDirectionLabel(angle) {
+            if (angle >= 22.5 && angle < 67.5) return "NW";
+            if (angle >= 67.5 && angle < 112.5) return "W";
+            if (angle >= 112.5 && angle < 157.5) return "SW";
+            if (angle >= 157.5 && angle < 202.5) return "S";
+            if (angle >= 202.5 && angle < 247.5) return "SE";
+            if (angle >= 247.5 && angle < 292.5) return "E";
+            if (angle >= 292.5 && angle < 337.5) return "NE";
+            return "N";
+        }
 
+        if (isAppleDevice()) {
+            window.addEventListener("deviceorientation", handleOrientationEvent);
+        } else {
+            window.addEventListener("deviceorientationabsolute", handleOrientationEvent, true);
+        }
 
-      // apply rotation to compass rose
-      if (typeof rose.style.transform !== "undefined") {
-        rose.style.transform = "rotateZ(" + positionCurrent.hng + "deg)";
-      } else if (typeof rose.style.webkitTransform !== "undefined") {
-        rose.style.webkitTransform = "rotateZ(" + positionCurrent.hng + "deg)";
-      }
-    } else {
-      // device can't show heading
+        document.addEventListener("DOMContentLoaded", function() {
 
-      positionHng.textContent = "n/a";
-      showHeadingWarning();
-    }
-  }
+            function supportsOrientation() {
+                return 'ondeviceorientation' in window || 'ondeviceorientationabsolute' in window;
+            }
 
-  function showHeadingWarning() {
-    if (!warningHeadingShown) {
-      popupOpen("noorientation");
-      warningHeadingShown = true;
-    }
-  }
+            function showNotSupportedMessage() {
+                msg.classList.remove("d-none");
+            }
 
-  function onFullscreenChange() {
-    if (isOrientationLockable && getBrowserFullscreenElement()) {
-      if (screen.orientation && screen.orientation.lock) {
-        screen.orientation.lock(getBrowserOrientation()).then(function () {
-        }).catch(function () {
+            if (supportsOrientation()) {
+                if (isAppleDevice()) {
+                    window.addEventListener("deviceorientation", handleOrientationEvent);
+                } else {
+                    window.addEventListener("deviceorientationabsolute", handleOrientationEvent, true);
+                }
+            } else {
+                showNotSupportedMessage();
+            }
+
+            if (localStorage.getItem('locationEnabled') === 'true') {
+                locationSwitch.checked = true;
+            } else {
+                locationSwitch.checked = false;
+            }
+
+            locationSwitch.addEventListener("change", function() {
+                if (this.checked && "geolocation" in navigator) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        const latitude = position.coords.latitude;
+                        const longitude = position.coords.longitude;
+                        coordsElem.textContent = `${latitude.toFixed(4)}, ${longitude.toFixed(4)}`;
+                        shareLinkElem.href =
+                            `https://www.google.com/maps/?q=${latitude},${longitude}`;
+                        locationElem.classList.remove('d-none');
+                    });
+                    localStorage.setItem('locationEnabled', 'true');
+                } else {
+                    coordsElem.textContent = '';
+                    shareLinkElem.href = '#';
+                    locationElem.classList.add('d-none');
+                    localStorage.setItem('locationEnabled', 'false');
+                }
+            });
+
+            function copyLocationLink() {
+                var textArea = document.createElement("textarea");
+                textArea.value = document.getElementById("chromeLink").textContent;
+                document.body.appendChild(textArea);
+                textArea.select();
+                document.execCommand("Copy");
+                textArea.remove();
+            }
+
+            document.getElementById("copyButton").addEventListener("click", copyLocationLink);
+
+            var html = document.querySelector('html');
+            var initialTheme = html.getAttribute('data-bs-theme');
+            compass.src = initialTheme === 'dark' ? 'https://onlinedigitalcompass.com/images/compass-light.png' : 'https://onlinedigitalcompass.com/images/compass-dark.png';
+
+            var observer = new MutationObserver(function(mutations) {
+                mutations.forEach(function(mutation) {
+                    if (mutation.attributeName === 'data-bs-theme') {
+                        var newTheme = html.getAttribute('data-bs-theme');
+                        compass.src = newTheme === 'dark' ? 'https://onlinedigitalcompass.com/images/compass-light.png' :
+                            'https://onlinedigitalcompass.com/images/compass-dark.png';
+                    }
+                });
+            });
+
+            var config = {
+                attributes: true,
+                attributeFilter: ['data-bs-theme']
+            };
+            observer.observe(html, config);
         });
-      }
-    } else {
-      lockOrientationRequest(false);
-    }
-  }
-
-  function toggleOrientationLockable(lockable) {
-    isOrientationLockable = lockable;
-
-    if (isOrientationLockable) {
-      btnLockOrientation.classList.remove("btn--hide");
-
-      btnNightmode.classList.add("column-25");
-      btnNightmode.classList.remove("column-33");
-      btnMap.classList.add("column-25");
-      btnMap.classList.remove("column-33");
-      btnInfo.classList.add("column-25");
-      btnInfo.classList.remove("column-33");
-    } else {
-      btnLockOrientation.classList.add("btn--hide");
-
-      btnNightmode.classList.add("column-33");
-      btnNightmode.classList.remove("column-25");
-      btnMap.classList.add("column-33");
-      btnMap.classList.remove("column-25");
-      btnInfo.classList.add("column-33");
-      btnInfo.classList.remove("column-25");
-    }
-  }
-
-  function checkLockable() {
-    if (screen.orientation && screen.orientation.lock) {
-      screen.orientation.lock(getBrowserOrientation()).then(function () {
-        toggleOrientationLockable(true);
-        browserUnlockOrientation();
-      }).catch(function (event) {
-        if (event.code === 18) { // The page needs to be fullscreen in order to call lockOrientation(), but is lockable
-          toggleOrientationLockable(true);
-          browserUnlockOrientation(); //needed as chrome was locking orientation (even if not in fullscreen, bug??)
-        } else {  // lockOrientation() is not available on this device (or other error)
-          toggleOrientationLockable(false);
-        }
-      });
-    } else {
-      toggleOrientationLockable(false);
-    }
-  }
-
-  function lockOrientationRequest(doLock) {
-    if (isOrientationLockable) {
-      if (doLock) {
-        browserRequestFullscreen();
-        lockOrientation(true);
-      } else {
-        browserUnlockOrientation();
-        browserExitFullscreen();
-        lockOrientation(false);
-      }
-    }
-  }
-
-  function lockOrientation(locked) {
-    if (locked) {
-      btnLockOrientation.classList.add("active");
-    } else {
-      btnLockOrientation.classList.remove("active");
-    }
-
-    isOrientationLocked = locked;
-  }
-
-  function toggleOrientationLock() {
-    if (isOrientationLockable) {
-      lockOrientationRequest(!isOrientationLocked);
-    }
-  }
-
-  function locationUpdate(position) {
-    positionCurrent.lat = position.coords.latitude;
-    positionCurrent.lng = position.coords.longitude;
-
-    positionLat.textContent = decimalToSexagesimal(positionCurrent.lat, "lat");
-    positionLng.textContent = decimalToSexagesimal(positionCurrent.lng, "lng");
-  }
-
-  function locationUpdateFail(error) {
-    positionLat.textContent = "n/a";
-    positionLng.textContent = "n/a";
-    console.log("location fail: ", error);
-  }
-
-  function setNightmode(on) {
-
-    if (on) {
-      btnNightmode.classList.add("active");
-    } else {
-      btnNightmode.classList.remove("active");
-    }
-
-    window.setTimeout(function() {
-      if (on) {
-        document.documentElement.classList.add("nightmode");
-      } else {
-        document.documentElement.classList.remove("nightmode");
-      }
-    }, 1);
-
-
-    isNightMode = on;
-  }
-
-  function toggleNightmode() {
-    setNightmode(!isNightMode);
-  }
-
-  function openMap() {
-    window.open("https://www.google.com/maps/place/@" + positionCurrent.lat + "," + positionCurrent.lng + ",16z", "_blank");
-  }
-
-  function popupOpenFromClick(event) {
-    popupOpen(event.currentTarget.dataset.name);
-  }
-
-  function popupOpen(name) {
-    var i;
-    for (i=0; i<popupInners.length; i++) {
-      popupInners[i].classList.add("popup__inner--hide");
-    }
-    document.getElementById("popup-inner-" + name).classList.remove("popup__inner--hide");
-
-    popup.classList.add("popup--show");
-  }
-
-  function popupClose() {
-    popup.classList.remove("popup--show");
-  }
-
-  function popupContentsClick(event) {
-    event.stopPropagation();
-  }
-
-  function decimalToSexagesimal(decimal, type) {
-    var degrees = decimal | 0;
-    var fraction = Math.abs(decimal - degrees);
-    var minutes = (fraction * 60) | 0;
-    var seconds = (fraction * 3600 - minutes * 60) | 0;
-
-    var direction = "";
-    var positive = degrees > 0;
-    degrees = Math.abs(degrees);
-    switch (type) {
-      case "lat":
-        direction = positive ? "N" : "S";
-        break;
-      case "lng":
-        direction = positive ? "E" : "W";
-        break;
-    }
-
-    return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
-  }
-
-  if (screen.width > screen.height) {
-    defaultOrientation = "landscape";
-  } else {
-    defaultOrientation = "portrait";
-  }
-  if (debug) {
-    debugOrientationDefault.textContent = defaultOrientation;
-  }
-
-  window.addEventListener("deviceorientation", onHeadingChange);
-
-  document.addEventListener("fullscreenchange", onFullscreenChange);
-  document.addEventListener("webkitfullscreenchange", onFullscreenChange);
-  document.addEventListener("mozfullscreenchange", onFullscreenChange);
-  document.addEventListener("MSFullscreenChange", onFullscreenChange);
-
-  btnLockOrientation.addEventListener("click", toggleOrientationLock);
-  btnNightmode.addEventListener("click", toggleNightmode);
-  btnMap.addEventListener("click", openMap);
-
-  var i;
-  for (i=0; i<btnsPopup.length; i++) {
-    btnsPopup[i].addEventListener("click", popupOpenFromClick);
-  }
-
-  popup.addEventListener("click", popupClose);
-  popupContents.addEventListener("click", popupContentsClick);
-
-  navigator.geolocation.watchPosition(locationUpdate, locationUpdateFail, {
-    enableHighAccuracy: false,
-    maximumAge: 30000,
-    timeout: 27000
-  });
-
-  setNightmode(false);
-  checkLockable();
-
-}());
