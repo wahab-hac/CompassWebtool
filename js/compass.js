958465ac6ae7d236e1391ef5a1cf238200a5c7fc
Abdul Wahab <86112828+abdulwahab-codehub@users.noreply.github.com>
2024-01-13 03:06:29 +0500

    Add files via upload

--- js/compass.js
+++ js/compass.js
@@ -52,80 +52,82 @@ if (isAppleDevice()) {
     window.addEventListener("deviceorientationabsolute", handleOrientationEvent, true);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    function supportsOrientation() {
-        return 'ondeviceorientation' in window || 'ondeviceorientationabsolute' in window;
-    }
+//     function supportsOrientation() {
+//         return 'ondeviceorientation' in window || 'ondeviceorientationabsolute' in window;
+//     }
 
-    function showNotSupportedMessage() {
-        msg.classList.remove("d-none");
-    }
+//     function showNotSupportedMessage() {
+//         msg.classList.remove("d-none");
+//     }
 
-    if (supportsOrientation()) {
-        if (isAppleDevice()) {
-            window.addEventListener("deviceorientation", handleOrientationEvent);
-        } else {
-            window.addEventListener("deviceorientationabsolute", handleOrientationEvent, true);
-        }
-    } else {
-        showNotSupportedMessage();
-    }
+//     if (supportsOrientation()) {
+//         if (isAppleDevice()) {
+//             window.addEventListener("deviceorientation", handleOrientationEvent);
+//         } else {
+//             window.addEventListener("deviceorientationabsolute", handleOrientationEvent, true);
+//         }
+//     } else {
+//         showNotSupportedMessage();
+//     }
 
-    if (localStorage.getItem('locationEnabled') === 'true') {
-        locationSwitch.checked = true;
-    } 
+//     if (localStorage.getItem('locationEnabled') === 'true') {
+//         locationSwitch.checked = true;
+//     } else {
+//         locationSwitch.checked = false;
+//     }
 
-    locationSwitch.addEventListener("change", function () {
-        if (this.checked && "geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                coordsElem.textContent = `${latitude.toFixed(4)}, ${longitude.toFixed(4)}`;
-                shareLinkElem.href =
-                    `https://www.google.com/maps/?q=${latitude},${longitude}`;
-                locationElem.classList.remove('d-none');
-            });
-            localStorage.setItem('locationEnabled', 'true');
-        } else {
-            coordsElem.textContent = '';
-            shareLinkElem.href = '#';
-            locationElem.classList.add('d-none');
-            localStorage.setItem('locationEnabled', 'false');
-        }
-    });
+//     locationSwitch.addEventListener("change", function () {
+//         if (this.checked && "geolocation" in navigator) {
+//             navigator.geolocation.getCurrentPosition(function (position) {
+//                 const latitude = position.coords.latitude;
+//                 const longitude = position.coords.longitude;
+//                 coordsElem.textContent = `${latitude.toFixed(4)}, ${longitude.toFixed(4)}`;
+//                 shareLinkElem.href =
+//                     `https://www.google.com/maps/?q=${latitude},${longitude}`;
+//                 locationElem.classList.remove('d-none');
+//             });
+//             localStorage.setItem('locationEnabled', 'true');
+//         } else {
+//             coordsElem.textContent = '';
+//             shareLinkElem.href = '#';
+//             locationElem.classList.add('d-none');
+//             localStorage.setItem('locationEnabled', 'false');
+//         }
+//     });
 
-    function copyLocationLink() {
-        var textArea = document.createElement("textarea");
-        textArea.value = document.getElementById("chromeLink").textContent;
-        document.body.appendChild(textArea);
-        textArea.select();
-        document.execCommand("Copy");
-        textArea.remove();
-    }
+//     function copyLocationLink() {
+//         var textArea = document.createElement("textarea");
+//         textArea.value = document.getElementById("chromeLink").textContent;
+//         document.body.appendChild(textArea);
+//         textArea.select();
+//         document.execCommand("Copy");
+//         textArea.remove();
+//     }
 
-    document.getElementById("copyButton").addEventListener("click", copyLocationLink);
+//     document.getElementById("copyButton").addEventListener("click", copyLocationLink);
 
-    var html = document.querySelector('html');
-    var initialTheme = html.getAttribute('data-bs-theme');
-    compass.src = initialTheme === 'dark' ? 'https://onlinedigitalcompass.com/images/compass-light.png' : 'https://onlinedigitalcompass.com/images/compass-dark.png';
+//     var html = document.querySelector('html');
+//     var initialTheme = html.getAttribute('data-bs-theme');
+//     compass.src = initialTheme === 'dark' ? 'https://onlinedigitalcompass.com/images/compass-light.png' : 'https://onlinedigitalcompass.com/images/compass-dark.png';
 
-    var observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            if (mutation.attributeName === 'data-bs-theme') {
-                var newTheme = html.getAttribute('data-bs-theme');
-                compass.src = newTheme === 'dark' ? 'https://onlinedigitalcompass.com/images/compass-light.png' :
-                    'https://onlinedigitalcompass.com/images/compass-dark.png';
-            }
-        });
-    });
+//     var observer = new MutationObserver(function (mutations) {
+//         mutations.forEach(function (mutation) {
+//             if (mutation.attributeName === 'data-bs-theme') {
+//                 var newTheme = html.getAttribute('data-bs-theme');
+//                 compass.src = newTheme === 'dark' ? 'https://onlinedigitalcompass.com/images/compass-light.png' :
+//                     'https://onlinedigitalcompass.com/images/compass-dark.png';
+//             }
+//         });
+//     });
 
-    var config = {
-        attributes: true,
-        attributeFilter: ['data-bs-theme']
-    };
-    observer.observe(html, config);
-});
+//     var config = {
+//         attributes: true,
+//         attributeFilter: ['data-bs-theme']
+//     };
+//     observer.observe(html, config);
+// });
 function showDialog() {
     let dialog = document.getElementById('dialog');
     dialog.classList.remove('hidden');
